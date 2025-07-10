2bc87ad0a7588da658ec687c6f0392e87d97465d
Félix Chénier <chenier.felix@uqam.ca>
2025-07-10 17:26:27 -0400

    Now send the Player's y rotation (as a dummy data) to a websocket server (as a first step toward a DBox driver that uses websocket)

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -3,6 +3,25 @@ extends RigidBody3D
 var LINEAR_SPEED: float = 3  # m/s
 var ANGULAR_SPEED: float = 3  # rad/s ?
 
+
+# Websocket stuff
+# The URL we will connect to.
+@export var websocket_url = "localhost:8765"
+# Our WebSocketClient instance.
+var socket = WebSocketPeer.new()
+
+
+func _ready():
+	# Initiate connection to the given URL.
+	var err = socket.connect_to_url(websocket_url)
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
+	else:
+		# Wait for the socket to connect.
+		await get_tree().create_timer(2).timeout
+
+
 # Called every frame
 func _physics_process(delta: float) -> void:
 	var desired_linear_velocity = Vector3.ZERO
@@ -24,3 +43,7 @@ func _physics_process(delta: float) -> void:
 
 	apply_force(1000 * (desired_linear_velocity - linear_velocity))
 	apply_torque(1000 * (desired_angular_velocity - angular_velocity))
+	
+	# Send new orientation to DBox
+	socket.send_text(str(rotation_degrees.y))
+	socket.poll()
