aa97a37da089ddb4e6e92c9a181d308f5aca3a13
Simon Manès <simonmanes.sm@gmail.com>
2025-07-15 13:39:36 -0400

    En plus propre.

--- src/Humans/WalkAnim.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D  # Ce script est optionnel si tu veux jouer une anim à l'init
-
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-
-func _ready():
-	anim_player.play("Walking")  # Nom exact de ton anim Mixamo
