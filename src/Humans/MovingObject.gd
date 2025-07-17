9e5b680daf8899d4dade9eb0ee3d78f3fe78d659
Simon Manès <simonmanes.sm@gmail.com>
2025-07-17 14:21:21 -0400

    Scène WalkingPedestrian Parent des autres scènes (Kate, josh et megan et bientot d'autres...). Ainsi on peut modifier le collider de detection qui se modifiera sur tous les humains. Par contre, les systeme animations sont propres aux models car ca s'applique à eux spécifiquement. Par contre les animations sont communes!

--- src/Humans/MovingObject.gd
+++ src/Humans/MovingObject.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@export var anim_tree: AnimationTree
 @export var max_speed : float = 2
 @export var acceleration : float = 1
 var current_speed : float
@@ -7,7 +8,6 @@ var target_speed : float
 
 @onready var detection_area: Area3D = $Area3D
 @onready var pathFollow: PathFollow3D = get_parent()
-@onready var anim_tree: AnimationTree = $AnimationTree
 var is_blocked : int = 0
 
 func _ready():
@@ -15,7 +15,6 @@ func _ready():
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if (body.is_in_group("NPC") or body.is_in_group("Player")):
-		print("detected!")
 		is_blocked += 1
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
@@ -28,6 +27,7 @@ func _process(delta):
 		target_speed = max_speed
 		current_speed = move_toward(current_speed, target_speed, delta * acceleration)
 	else:
+		print("detected")
 		current_speed = 0
 
 	anim_tree.set("parameters/blend_position", current_speed/target_speed)
