a7de76ba6993975550e63d4b1f76868e73869239
Simon Manès <simonmanes.sm@gmail.com>
2025-07-17 11:39:01 -0400

    Moving Object First Step Pedestrians First anims (walk and idle)

---
+++ src/Humans/MovingObject.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+@export var max_speed : float = 2
+@export var acceleration : float = 1
+var current_speed : float
+var target_speed : float
+
+@onready var detection_area: Area3D = $Area3D
+@onready var pathFollow: PathFollow3D = get_parent()
+@onready var anim_tree: AnimationTree = $AnimationTree
+var is_blocked : int = 0
+
+func _ready():
+	anim_tree.active = true
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if (body.is_in_group("NPC") or body.is_in_group("Player")):
+		print("detected!")
+		is_blocked += 1
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if (body.is_in_group("NPC") or body.is_in_group("Player")):
+		is_blocked -= 1
+	
+func _process(delta):
+	# There is speed lerp only for going on walk, not for stopping
+	if (is_blocked < 1):
+		target_speed = max_speed
+		current_speed = move_toward(current_speed, target_speed, delta * acceleration)
+	else:
+		current_speed = 0
+
+	anim_tree.set("parameters/blend_position", current_speed/target_speed)
+	pathFollow.progress += current_speed * delta
