86515b5102ba6ca34a8c7f9244abd094911f75ac
Korn666 <simonmanes.sm@gmail.com>
2026-03-12 12:45:37 +0100

    Sound design and music mock up

--- src/games/races/obstacle_race/game/scripts/obstacle_race_game.gd
+++ src/games/races/obstacle_race/game/scripts/obstacle_race_game.gd
@@ -7,6 +7,11 @@ class_name ObstacleRaceGame
 @export var fail_point_val = 5.0
 @export var score_popup_scene: PackedScene
 @export var score_label: Label
+@export var challenge_success_sound: AudioStreamWAV
+@export var challenge_fail_sound: AudioStreamWAV
+@export var race_success_sound: AudioStreamWAV
+@onready var sfx_player: AudioStreamPlayer = $sfx_player
+@onready var music_player: AudioStreamPlayer = $music_player
 var races_data: Array[RaceData]
 var current_race_data: RaceData
 var current_race_data_i : int = 0
@@ -16,7 +21,9 @@ func init(_races_data: Array[RaceData])->void:
 	randomize()
 	races_data = _races_data
 	current_race_data = races_data[0]
-	add_score(0)	
+	add_score(0)
+	if is_instance_valid(music_player) and is_instance_valid(music_player.stream) :
+		music_player.play()
 
 func add_score(score: int = 0):
 	var popup: ScorePopup = score_popup_scene.instantiate()
@@ -27,10 +34,15 @@ func add_score(score: int = 0):
 	total_score += score
 	score_label.text = str(total_score)
 	
+func play_sfx(audio: AudioStreamWAV)->void:
+	sfx_player.stream = audio
+	sfx_player.play()
+	
 func on_obstacle_collision(body: Area3D, area3D_emitter: Area3D)->void:
 	if body.is_in_group("Player"):
 		current_race_data.score -= fail_point_val
 		add_score(-fail_point_val)
+		play_sfx(challenge_fail_sound)
 		obstacle_collision = true
 		area3D_emitter.set_deferred("monitoring", false)
 	
@@ -45,6 +57,7 @@ func on_challenge_area_exited(body: Area3D, area3D_emitter: Area3D)->void:
 	on_challenge = false
 	if not obstacle_collision:
 		add_score(success_point_val)
+		play_sfx(challenge_success_sound)
 	area3D_emitter.set_deferred("monitoring", false)
 	
 func on_race_entered(body: Area3D)->void:
@@ -57,4 +70,5 @@ func on_race_entered(body: Area3D)->void:
 func on_race_exited(body: Area3D, area3D_emitter: Area3D)->void:
 	if not body.get_parent().is_in_group("Player"):
 		return
+	play_sfx(race_success_sound)
 	area3D_emitter.set_deferred("monitoring", false)
