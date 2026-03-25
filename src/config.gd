cb321c6a85e11fc7f89dfc52026bc5abafde4ddd
Laboratoire <lab@felixchenier.com>
2026-03-25 17:00:29 -0400

    - ajout d’une fenêtre supplémentaire (GUI) à la scène biofeedback_optitrack pour pointer les centres des roues et les mains - ajout des paramètres des coordonnées des centres des roues et des mains dans config - modification du script biofeedback_optitrack pour prendre comme données d’entrée les coordonnées du centre des roues dans le repère local du simulateur et non plus dans le repère global - ajout d’un offset pour le trail illustrant la trajectoire de la main par rapport aux repères des clusters de l’avant-bras
    https://github.com/WheelSims/wheelsims/issues/28

--- src/config.gd
+++ src/config.gd
@@ -45,7 +45,9 @@ var _defaults: Dictionary[String, Dictionary] = {
 	"devices.optitrack.enabled": {"order": 3.7, "label": "Optitrack", "type": "bool", "default": false},
 
 	"coordinates.left_wheel_center": {"order":4.01, "label": "Left wheel center", "type": "array", "default": [0.0, 0.0, 0.0], "reference": "simulator"},
-	"coordinates.right_wheel_center": {"order":4.02, "label": "Left wheel center", "type": "array", "default": [0.0, 0.0, 0.0], "reference": "simulator"},
+	"coordinates.right_wheel_center": {"order":4.02, "label": "Right wheel center", "type": "array", "default": [0.0, 0.0, 0.0], "reference": "simulator"},
+	"coordinates.left_hand": {"order":4.03, "label": "Left hand", "type": "array", "default": [0.0, 0.0, 0.0], "reference": "forearm_cluster_left"},
+	"coordinates.right_hand": {"order":4.04, "label": "Right hand", "type": "array", "default": [0.0, 0.0, 0.0], "reference": "forearm_cluster_right"},
 }
 
 ## Overrides
