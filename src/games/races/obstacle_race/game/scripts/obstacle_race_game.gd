c40f196bdcea18306fd9c5b4b093b095419945cf
Korn666 <simonmanes.sm@gmail.com>
2026-03-25 18:20:20 +0100

    Better font for timers. Crowd desactivaton to improve fps.

--- src/games/races/obstacle_race/game/scripts/obstacle_race_game.gd
+++ src/games/races/obstacle_race/game/scripts/obstacle_race_game.gd
@@ -37,8 +37,8 @@ func _process(delta: float) -> void:
 		_update_labels()
 	
 func _update_labels()->void:
-	lap_time_label.text = "%.1f s" % lap_timer
-	total_time_label.text = "%.1f s" % total_timer
+	lap_time_label.text = "%.1f" % lap_timer
+	total_time_label.text = "%.1f" % total_timer
 
 func add_score(score: int = 0):
 	var popup: ScorePopup = score_popup_scene.instantiate()
@@ -74,20 +74,34 @@ func on_challenge_area_exited(body: Area3D, area3D_emitter: Area3D)->void:
 		play_sfx(challenge_success_sound)
 	area3D_emitter.set_deferred("monitoring", false)
 	
-func on_race_entered(body: Area3D)->void:
+func on_race_entered(body: Area3D, area3D_emitter: Area3D)->void:
 	if not body.is_in_group("Player"):
 		return
 	race_on_pause = false
 	lap_timer = 0
+	
 	if current_race_data_i < races_data.size()-1:
 		current_race_data_i += 1
 		current_race_data = races_data[current_race_data_i]
 		current_race_data.score = 0
-		
+		set_crowds_visible(current_race_data.final_crowds, true)
+	if current_race_data_i < races_data.size()-1:
+		set_crowds_visible(races_data[current_race_data_i+1].start_crowds, true)
+	area3D_emitter.set_deferred("monitoring", false)
+	
 func on_race_exited(body: Area3D, area3D_emitter: Area3D)->void:
 	if not body.get_parent().is_in_group("Player"):
 		return
 	current_race_data.lap_time = lap_timer
 	race_on_pause = true
 	play_sfx(race_success_sound)
+	set_crowds_visible(races_data[current_race_data_i].start_crowds, false)
+	if current_race_data_i > 0:
+		set_crowds_visible(races_data[current_race_data_i-1].final_crowds, false)
 	area3D_emitter.set_deferred("monitoring", false)
+	
+func set_crowds_visible(crowds: Array[Crowd], visible:bool)->void:
+	if crowds.size() == 0:
+		return
+	for crowd in crowds:
+		crowd.visible = visible
