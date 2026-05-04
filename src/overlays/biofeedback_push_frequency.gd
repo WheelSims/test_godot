767b17952cda99bccb83a3e2f9dd54303d0ea4ba
ACER_ALEX\adjim <adjimi.alexandre.pro@gmail.com>
2026-05-04 21:42:22 +0200

    [biofeedback_push_frequency] - fusion des scripts de la scène -> 1 seul script - coding standard - amélioration du code en utilisant les @exports objets depuis l'inspecteur #40

--- src/overlays/biofeedback_push_frequency.gd
+++ src/overlays/biofeedback_push_frequency.gd
@@ -1,6 +1,44 @@
-extends Node2D
+extends Control
 
-func _process(_delta: float) -> void:
+var pos_init_slider
+@export_category("Slider parameters")
+@export var min_value = 0.4
+@export var max_value = 2.5
+
+@export var min_target_value = 1.0
+@export var max_target_value = 2.0
+
+var value = 0.6
+
+@export_category("Nodes")
+@export var node_min_value: Node
+@export var node_max_value: Node
+@export var node_min_target_value: Node
+@export var node_max_target_value: Node
+@export var node_value: Node
+@export var node_slider_zone: Node
+@export var node_slider: Node
+@export var node_target_zone: Node
+@export var node_green_zone: Node
+
+
+func _process(_delta) -> void:
+
+	node_min_value.text = str(min_value)
+	node_max_value.text = str(max_value)
+	node_value.text = str(snappedf(value, 0.1))
+
+	node_slider.position.y = node_slider_zone.size.y - (value - min_value) * node_slider_zone.size.y / (max_value-min_value)
+	node_slider.size.x = node_slider_zone.size.x
+	
+	node_target_zone.position.y = node_slider_zone.size.y - (min_target_value - min_value) * node_slider_zone.size.y / (max_value-min_value)
+	node_green_zone.size.y = node_slider_zone.size.y * (max_target_value - min_target_value) / (max_value - min_value) + node_slider_zone.size.y * (max_target_value - max_target_value)
+
+	node_min_target_value.text = str(min_target_value)
+	node_max_target_value.text = str(max_target_value)
+	
+	node_max_target_value.position.y = node_green_zone.size.y
+
 	# Should we quit
 	if not Config.get_value("overlays.biofeedback_push_frequency.enabled"):
 		queue_free()
