5018766e28145d813676a4e4330e35bb5d40b5f6
Félix Chénier <chenier.felix@uqam.ca>
2026-06-08 12:03:33 -0400

    [#49-PascalCase-Node-Naming] Devices done

--- src/devices/screens/front_floor_screens.gd
+++ src/devices/screens/front_floor_screens.gd
@@ -7,8 +7,8 @@ func _ready():
 	# Assign the game viewport to the main window
 	main.scene_viewport.size.x = 1280
 	main.scene_viewport.size.y = 768*2
-	get_node("front_window/texture_rect").texture = main.scene_viewport.get_texture()
-	get_node("floor_window/texture_rect").texture = main.scene_viewport.get_texture()
+	get_node("FrontWindow/TextureRect").texture = main.scene_viewport.get_texture()
+	get_node("FloorWindow/TextureRect").texture = main.scene_viewport.get_texture()
 	
 func _process(_delta):
 	if Config.value_changed("front_floor_screens", "devices.screens.front_floor_screens.full_screen.enabled"):
@@ -16,20 +16,20 @@ func _process(_delta):
 			var screen_count = DisplayServer.get_screen_count()
 			var screen_index = min(screen_count, Config.get_value("devices.screens.front_floor_screens.full_screen.front_screen_index")-1)
 			screen_index = max(0, screen_index)
-			$front_window.set_current_screen(screen_index)
-			$front_window.mode = Window.MODE_FULLSCREEN
+			$FrontWindow.set_current_screen(screen_index)
+			$FrontWindow.mode = Window.MODE_FULLSCREEN
 
 
 			screen_index = min(screen_count, Config.get_value("devices.screens.front_floor_screens.full_screen.floor_screen_index")-1)
 			screen_index = max(0, screen_index)
-			$floor_window.set_current_screen(screen_index)
-			$floor_window.mode = Window.MODE_FULLSCREEN
+			$FloorWindow.set_current_screen(screen_index)
+			$FloorWindow.mode = Window.MODE_FULLSCREEN
 
 		else:
-			$front_window.mode = Window.MODE_WINDOWED
-			$front_window.set_current_screen(0)
-			$floor_window.mode = Window.MODE_WINDOWED
-			$floor_window.set_current_screen(0)
+			$FrontWindow.mode = Window.MODE_WINDOWED
+			$FrontWindow.set_current_screen(0)
+			$FloorWindow.mode = Window.MODE_WINDOWED
+			$FloorWindow.set_current_screen(0)
 
 	if not Config.get_value("devices.screens.front_floor_screens.enabled"):
 		queue_free()
--- src/devices/screens/single_screen.gd
+++ src/devices/screens/single_screen.gd
@@ -7,7 +7,7 @@ func _ready():
 	# Assign the game viewport to the main window
 	main.scene_viewport.size.x = 1280
 	main.scene_viewport.size.y = 768
-	get_node("main_window/texture_rect").texture = main.scene_viewport.get_texture()
+	get_node("MainWindow/TextureRect").texture = main.scene_viewport.get_texture()
 
 
 func _process(_delta):
@@ -19,10 +19,10 @@ func _process(_delta):
 			var screen_count = DisplayServer.get_screen_count()
 			var screen_index = min(screen_count, Config.get_value("devices.screens.single_screen.full_screen.screen_index") - 1)
 			screen_index = max(0, screen_index)
-			$main_window.set_current_screen(screen_index)
-			$main_window.mode = Window.MODE_FULLSCREEN
+			$MainWindow.set_current_screen(screen_index)
+			$MainWindow.mode = Window.MODE_FULLSCREEN
 		else:
-			$main_window.mode = Window.MODE_WINDOWED
+			$MainWindow.mode = Window.MODE_WINDOWED
 
 	if not Config.get_value("devices.screens.single_screen.enabled"):
 		queue_free()
